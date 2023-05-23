f3822e0d65269927649ca7d72e0b755d7cab5030
Luiz Magno <luiz.magno7@yahoo.com.br>
2023-05-22 22:28:21 -0300

    feat: create user and pagination to list users

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,42 @@
+import { Input } from "@/components/Form/Input";
+import { Header } from "@/components/Header";
+import { Sidebar } from "@/components/Sidebar";
+import { Box, Button, Checkbox, Divider, Flex, HStack, Heading, Icon, SimpleGrid, Table, Tbody, Td, Text, Th, Thead, Tr, VStack } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+export default function createUser() {
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+                <Sidebar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+
+                    <Divider my="6" borderColor="gray.700" />
+
+                    <VStack spacing="8">
+                        <SimpleGrid minChildWidth="248px" spacing="8" w="100%">
+                            <Input name="name" label="Nome completo" />
+                            <Input name="email" type="email" label="E-mail" />
+                        </SimpleGrid>
+
+                        <SimpleGrid minChildWidth="248px" spacing="8" w="100%">
+                            <Input name="password" type="password" label="Senha" />
+                            <Input name="password_confirmation" type="password" label="Confirmação de senha" />
+                        </SimpleGrid>
+                    </VStack>
+
+                    <Flex mt="8" justify="flex-end">
+                        <HStack spacing="4">
+                            <Button colorScheme="whiteAlpha">Cancelar</Button>
+                            <Button colorScheme="pink">Salvar</Button>
+                        </HStack>
+                    </Flex>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
